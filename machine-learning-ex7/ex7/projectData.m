415210a9ef25b48f61dc42e1ee4d2a9e63937fb1
Minhyuk Kim <newjoseph96@gmail.com>
2021-06-29 15:36:03 +0900

    Project Data (PCA)

--- machine-learning-ex7/ex7/projectData.m
+++ machine-learning-ex7/ex7/projectData.m
@@ -17,9 +17,15 @@ Z = zeros(size(X, 1), K);
 %                    x = X(i, :)';
 %                    projection_k = x' * U(:, k);
 %
+m = size(X,1);
 
-
-
+for i = 1:m
+  for k = 1:K
+    x = X(i, :)';
+    projection_k = x' * U(:,k);
+    Z(i,k) = projection_k;
+  endfor
+end
 
 % =============================================================
 
