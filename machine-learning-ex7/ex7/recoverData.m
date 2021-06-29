ca95a91bc566787703b4403f2262bc3e2bee5028
Minhyuk Kim <newjoseph96@gmail.com>
2021-06-29 15:55:08 +0900

    Recover Data (PCA)

--- machine-learning-ex7/ex7/recoverData.m
+++ machine-learning-ex7/ex7/recoverData.m
@@ -21,7 +21,15 @@ X_rec = zeros(size(Z, 1), size(U, 1));
 %               Notice that U(j, 1:K) is a row vector.
 %               
 
-
+m = size(Z,1);
+for i = 1:m
+  for j = 1:size(U,1);
+    v = Z(i,:)';
+    recovered_j = v' * U(j, 1:K)';
+    X_rec(i,j) = recovered_j;
+   endfor
+end
+    
 
 % =============================================================
 
